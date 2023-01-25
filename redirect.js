1e3a6d3b01fc8f50ce1076d1b5fae39368508644
sudh8684 <you@example.com>
2023-01-25 23:19:37 +0530

    parsing using express

--- redirect.js
+++ redirect.js
@@ -1,9 +1,25 @@
 const http= require('http')
-const connect=require('./connect')
-console.log(connect.someText)
-const server=http.createServer(connect.handler)
+const express=require('express')
+const bodyParser=require('body-parser')
+const app= express()
+
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.use('/addProducts',(req,res,next)=>{
+res.send('<form action="/product"method="POST"><input type="text"name="product name"><input type="text" name="size"><button type="submit">add product</button></form>')
+})
+
+app.use('/product',(req,res,next)=>{
+    console.log(req.body)
+    res.redirect('/' ) 
+    })
+
+    app.use('/',(req,res,next)=>{
+        console.log('product added')
+        res.send('<h1>Product added!</h1>' ) 
+        })
 
 
- 
- 
+const server=http.createServer(app)
+
 server.listen(3000)
